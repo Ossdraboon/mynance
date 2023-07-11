714a7e0e281c3e422aeec640ad8c49e40a758b1b
ossdraboon <ossdraboon@gmail.com>
2023-07-11 11:45:31 +0200

    createAccountPage layout and design finished.

--- lib/Widgets/Layouts/TextField.dart
+++ lib/Widgets/Layouts/TextField.dart
@@ -18,40 +18,42 @@ class Textfield extends StatefulWidget {
 class _TextfieldState extends State<Textfield> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-          colors: [
-            Colors.white,
-            Colors.blue.withOpacity(0.9),
-            Colors.blue.withOpacity(0.9),
-            Colors.lightBlue.withOpacity(0.8),
-            Colors.cyan.withOpacity(0.1),
-          ],
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [
+              Colors.white,
+              Colors.blue.withOpacity(0.9),
+              Colors.blue.withOpacity(0.9),
+              Colors.lightBlue.withOpacity(0.8),
+              Colors.cyan.withOpacity(0.1),
+            ],
+          ),
+          // color: const Color.fromRGBO(
+          //     15, 165, 210, 0.9),
+          border: Border.all(
+            color: const Color.fromRGBO(21, 80, 199, 0.3),
+            width: 0,
+          ),
+          borderRadius: BorderRadius.circular(10.0),
         ),
-        // color: const Color.fromRGBO(
-        //     15, 165, 210, 0.9),
-        border: Border.all(
-          color: const Color.fromRGBO(21, 80, 199, 0.3),
-          width: 0,
-        ),
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      height: 70,
-      width: 600,
-      child: TextFormField(
-        style: const TextStyle(color: Colors.white, fontSize: 25),
-        decoration: InputDecoration(
-          border: InputBorder.none,
-            labelText: widget._hint,
-            labelStyle: const TextStyle(color: Colors.white, fontSize: 18.0),
-            iconColor: Colors.blueAccent,
-            icon: Icon(widget._icon),
 
-            hintText: widget._hint,
-          hintStyle: const TextStyle(color: Colors.white, fontSize: 18.0),
+        child: TextFormField(
+          style: const TextStyle(color: Colors.white, fontSize: 25),
+          decoration: InputDecoration(
+            border: InputBorder.none,
+              labelText: widget._hint,
+              labelStyle: const TextStyle(color: Colors.white, fontSize: 18.0),
+              iconColor: Colors.blueAccent,
+              icon: Icon(widget._icon),
+
+              hintText: widget._hint,
+            hintStyle: const TextStyle(color: Colors.white, fontSize: 18.0),
+          ),
         ),
       ),
     );
--- lib/createAccountPage.dart
+++ lib/createAccountPage.dart
@@ -9,13 +9,12 @@ import 'Widgets/Layouts/TextFieldPassword.dart';
 import 'main.dart';
 
 class CreateAccount extends StatelessWidget {
-   CreateAccount({super.key});
-  final TextEditingController emailController =  TextEditingController();
-   final TextEditingController passwordController =  TextEditingController();
+  CreateAccount({super.key});
 
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
-
-   @override
+  @override
   Widget build(BuildContext context) {
     //var colorScheme = Theme.of(context).colorScheme;
 
@@ -38,9 +37,21 @@ class CreateAccount extends StatelessWidget {
                     children: <Widget>[
                       const SizedBox(height: 30),
                       Textfield(hint: "Name", icon: Icons.person),
-                      TextfieldPassword(hint: "Password",controller: passwordController ,icon: Icons.password),
-                      TextfieldPassword(hint: "Repeat Password",controller: passwordController ,icon: Icons.password), // TODO: P3, 23.07.04, Show password (toggle)
-                      TextfieldEmail(hint: "Email", icon: Icons.alternate_email, controller: emailController,), // TODO: P3, 23.07.04, Regex for Emailvalidation
+                      TextfieldPassword(
+                          hint: "Password",
+                          controller: passwordController,
+                          icon: Icons.password),
+                      TextfieldPassword(
+                          hint: "Repeat Password",
+                          controller: passwordController,
+                          icon: Icons.password),
+                      // TODO: P3, 23.07.04, Show password (toggle)
+                      TextfieldEmail(
+                        hint: "Email",
+                        icon: Icons.alternate_email,
+                        controller: emailController,
+                      ),
+                      // TODO: P3, 23.07.04, Regex for Emailvalidation
                       NumberfieldPhone(
                           hint: "Phone Number", icon: Icons.phone_android),
                       const SizedBox(height: 20),
@@ -114,48 +125,13 @@ class CreateAccount extends StatelessWidget {
                       Column(
                         children: <Widget>[
                           const SizedBox(height: 100),
-                          Textfield(hint: "Name", icon: Icons.person),
-                          TextfieldPassword(hint: "Password",controller: passwordController ,icon: Icons.password),
-                          TextfieldPassword(
-                              hint: "Repeat Password",controller: passwordController ,icon: Icons.password),
-                          TextfieldEmail(hint: "Email",controller: emailController ,icon: Icons.alternate_email),
-                          NumberfieldPhone(
-                              hint: "Phone Number",
-                              icon: Icons.phone_android_outlined),
-                          const SizedBox(height: 20),
-                          // const Row(
-                          //   children: [
-                          //     Icon(Icons.calendar_month),
-                          //     SizedBox(
-                          //       height: 30,
-                          //       width: 18,
-                          //     ),
-                          //     BirthDate(),
-                          //   ],
-                          // ),
-                          SizedBox(height: 10,),
-                          Row(
-                            children: [
-                              const Icon(Icons.transgender),
-                              const SizedBox(
-                                height: 30,
-                                width: 18,
-                              ),
-                              SizedBox(
-                                height: 70,
-                                width: 160,
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    border: Border.all(
-                                        color: Colors.black, width: 2),
-                                    borderRadius: const BorderRadius.all(
-                                        Radius.circular(10)),
-                                  ),
-                                  child: const GenderPick(),
-                                ),
-                              ),
-                            ],
-                          ),
+                          NameSection(),
+                          PasswordSection(passwordController: passwordController),
+                          PasswordRepeatSection(passwordController: passwordController),
+                          EmailSection(emailController: emailController),
+                          PhoneNumberSection(),
+                          const BirthdaySection(),
+                          const GenderSection(),
                           const SizedBox(height: 40),
                           const Row(
                             children: <Widget>[
@@ -180,6 +156,218 @@ class CreateAccount extends StatelessWidget {
   }
 }
 
+class NameSection extends StatelessWidget {
+  const NameSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Textfield(hint: "Name", icon: Icons.person),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: 20,
+            alignment: Alignment.topRight,
+            child: const Text(
+              "mandatory",
+              style: TextStyle(color: Colors.blueAccent),
+              textAlign: TextAlign.right,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class PasswordSection extends StatelessWidget {
+  const PasswordSection({
+    super.key,
+    required this.passwordController,
+  });
+
+  final TextEditingController passwordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        TextfieldPassword(
+            hint: "Password",
+            controller: passwordController,
+            icon: Icons.password),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: 20,
+            alignment: Alignment.topRight,
+            child: const Text(
+              "mandatory",
+              style: TextStyle(color: Colors.blueAccent),
+              textAlign: TextAlign.right,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class PasswordRepeatSection extends StatelessWidget {
+  const PasswordRepeatSection({
+    super.key,
+    required this.passwordController,
+  });
+
+  final TextEditingController passwordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        TextfieldPassword(
+            hint: "Repeat Password",
+            controller: passwordController,
+            icon: Icons.password),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: 20,
+            alignment: Alignment.topRight,
+            child: const Text(
+              "mandatory",
+              style: TextStyle(color: Colors.blueAccent),
+              textAlign: TextAlign.right,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class EmailSection extends StatelessWidget {
+  const EmailSection({
+    super.key,
+    required this.emailController,
+  });
+
+  final TextEditingController emailController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        TextfieldEmail(
+            hint: "Email",
+            controller: emailController,
+            icon: Icons.alternate_email),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: 20,
+            alignment: Alignment.topRight,
+            child: const Text(
+              "mandatory",
+              style: TextStyle(color: Colors.blueAccent),
+              textAlign: TextAlign.right,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class PhoneNumberSection extends StatelessWidget {
+  const PhoneNumberSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        NumberfieldPhone(
+            hint: "Phone Number",
+            icon: Icons.phone_android_outlined),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: 20,
+            alignment: Alignment.topRight,
+            child: const Text(
+              "optional",
+              style: TextStyle(color: Colors.blueAccent),
+              textAlign: TextAlign.right,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class GenderSection extends StatelessWidget {
+  const GenderSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [
+              Colors.white,
+              Colors.blue.withOpacity(0.9),
+              Colors.blue.withOpacity(0.9),
+              Colors.lightBlue.withOpacity(0.8),
+              Colors.cyan.withOpacity(0.1),
+            ],
+          ),
+          // color: const Color.fromRGBO(
+          //     15, 165, 210, 0.9),
+          border: Border.all(
+            color: const Color.fromRGBO(21, 80, 199, 0.3),
+            width: 0,
+          ),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(Icons.transgender, color: Colors.blueAccent),
+                SizedBox(width: 5),
+                GenderPick(),
+              ],
+            ),
+            Container(
+              height: 70,
+              alignment: Alignment.topRight,
+              child: const Text(
+                "optional",
+                style: TextStyle(color: Colors.blueAccent),
+                textAlign: TextAlign.right,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 const List<String> list = <String>['Auto', 'Male', 'Female', 'Other'];
 
 class GenderPick extends StatefulWidget {
@@ -194,23 +382,127 @@ class _GenderPickState extends State<GenderPick> {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      icon: const Icon(Icons.select_all_rounded),
-      style: const TextStyle(color: Colors.black),
-      alignment: Alignment.center,
-      onChanged: (String? value) {
-        // This is called when the user selects an item.
-        setState(() {
-          dropdownValue = value!;
-        });
-      },
-      items: list.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+        alignment: Alignment.center,
+        height: 60,
+        width: 200,
+        child: DropdownButton<String>(
+          iconSize: 0.0,
+          value: dropdownValue,
+          isExpanded: true,
+          underline: Container(),
+          dropdownColor: Colors.transparent,
+          elevation: 0,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+          alignment: Alignment.center,
+          onChanged: (String? value) {
+            // This is called when the user selects an item.
+            setState(() {
+              dropdownValue = value!;
+            });
+          },
+          items: list.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: SizedBox(
+                child: Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: [
+                          Colors.white,
+                          Colors.blue.withOpacity(0.9),
+                          Colors.blue.withOpacity(0.9),
+                          Colors.lightBlue.withOpacity(0.8),
+                          Colors.cyan.withOpacity(0.1),
+                        ],
+                      ),
+                      border: Border.all(
+                        color: Colors.white.withOpacity(0.5),
+                        // color: const Color.fromRGBO(21, 80, 199, 0.3),
+                        width: 0,
+                      ),
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.white.withOpacity(0.7),
+                          spreadRadius: 1,
+                          blurRadius: 7,
+                          offset:
+                              const Offset(2, 2), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Text(value)),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class BirthdaySection extends StatelessWidget {
+  const BirthdaySection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [
+              Colors.white,
+              Colors.blue.withOpacity(0.9),
+              Colors.blue.withOpacity(0.9),
+              Colors.lightBlue.withOpacity(0.8),
+              Colors.cyan.withOpacity(0.1),
+            ],
+          ),
+          // color: const Color.fromRGBO(
+          //     15, 165, 210, 0.9),
+          border: Border.all(
+            color: const Color.fromRGBO(21, 80, 199, 0.3),
+            width: 0,
+          ),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Stack(
+          children: [
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(
+                  Icons.calendar_month,
+                  color: Colors.blueAccent,
+                ),
+                BirthDate(),
+              ],
+            ),
+            Container(
+              alignment: Alignment.topRight,
+              child: const Text(
+                "optional",
+                style: TextStyle(color: Colors.blueAccent),
+                textAlign: TextAlign.right,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -230,8 +522,7 @@ class _BirthDateState extends State<BirthDate> with RestorationMixin {
   @override
   String? get restorationId => widget.restorationId;
 
-  final RestorableDateTime _selectedDate =
-      RestorableDateTime(DateTime.now());
+  final RestorableDateTime _selectedDate = RestorableDateTime(DateTime.now());
   late final RestorableRouteFuture<DateTime?> _restorableDatePickerRouteFuture =
       RestorableRouteFuture<DateTime?>(
     onComplete: _selectDate,
@@ -283,18 +574,55 @@ class _BirthDateState extends State<BirthDate> with RestorationMixin {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 70,
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
       child: Container(
+        height: 50,
+        width: 200,
         decoration: BoxDecoration(
-          border: Border.all(color: Colors.black, width: 2),
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [
+              Colors.white,
+              Colors.blue.withOpacity(0.9),
+              Colors.blue.withOpacity(0.9),
+              Colors.lightBlue.withOpacity(0.8),
+              Colors.cyan.withOpacity(0.1),
+            ],
+          ),
+          // color: const Color.fromRGBO(
+          //     15, 165, 210, 0.9),
+          border: Border.all(
+            color: Colors.white.withOpacity(0.5),
+            // color: const Color.fromRGBO(21, 80, 199, 0.3),
+            width: 0,
+          ),
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.white.withOpacity(0.7),
+              spreadRadius: 1,
+              blurRadius: 7,
+              offset: const Offset(2, 2), // changes position of shadow
+            ),
+          ],
         ),
-        child: TextButton(
-          onPressed: () {
-            _restorableDatePickerRouteFuture.present();
-          },
-          child: const Text('Pick your Birthday *'),
+        child: Row(
+          children: [
+            TextButton(
+              onPressed: () {
+                _restorableDatePickerRouteFuture.present();
+              },
+              child: const Text(
+                'Pick your Birthday',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.normal),
+              ),
+            ),
+          ],
         ),
       ),
     );
